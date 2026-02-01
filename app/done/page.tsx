d8ad7805b3b9aec527c33e976f0e4e04b0ef17a9
n70552300-svg <n70552300@gmail.com>
2026-02-01 11:05:43 -0800

    Create page.tsx

---
+++ app/done/page.tsx
@@ -0,0 +1,12 @@
+export default function DonePage() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Done</h1>
+      <p>One less weak link.</p>
+
+      <p>
+        <a href="/welcome">Run again</a>
+      </p>
+    </main>
+  );
+}
