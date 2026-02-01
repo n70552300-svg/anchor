266313f0bf27649a588ea53f5d738ac2344e0b49
n70552300-svg <n70552300@gmail.com>
2026-01-31 21:50:07 -0800

    Create index.js

---
+++ app/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Anchor</h1>
+      <p>Your oldest accounts are your weakest security link.</p>
+    </main>
+  );
+}
