03ba51d94308f28437086e368e0b7ceb5d988b6b
n70552300-svg <n70552300@gmail.com>
2026-01-31 21:20:40 -0800

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>Anchor</h1>
-      <p>Your oldest accounts are your weakest security link.</p>
-    </main>
-  );
-}
