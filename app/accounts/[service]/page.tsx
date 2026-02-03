230f860c33b11365673f30651b019fc9985644fc
n70552300-svg <n70552300@gmail.com>
2026-02-02 17:09:51 -0800

    Update page.tsx
    Recreate account detail page

--- app/accounts/[service]/page.tsx
+++ app/accounts/[service]/page.tsx
@@ -1,1 +1,35 @@
+import { sampleAccounts } from "../../_data/sampleAccounts";
 
+type Props = {
+  params: { service: string };
+};
+
+export default function AccountDetailPage({ params }: Props) {
+  const account = sampleAccounts.find(
+    (a) => a.slug === params.service
+  );
+
+  if (!account) {
+    return <p>Account not found.</p>;
+  }
+
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>{account.name}</h1>
+
+      <p>{account.why}</p>
+
+      <h2>Signals</h2>
+      <ul>
+        {account.signals.map((signal) => (
+          <li key={signal}>{signal}</li>
+        ))}
+      </ul>
+
+      <p>
+        <strong>Recommended next step:</strong>{" "}
+        {account.recommendedNextStep}
+      </p>
+    </main>
+  );
+}
